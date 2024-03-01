71d0a7a3a463c7f79082a1350741f60fd1bde706
YashwanthK <yashwanthkothakota@gmail.com>
2024-03-01 14:31:49 +0530

    initial backend setup along with env variables setup with envalid

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import app from './app';
+import env from './utils/validateEnv';
+import mongoose from 'mongoose';
+
+const port = env.PORT;
+
+mongoose
+  .connect(env.MONGODB_CONNECTION_STRING)
+  .then(() => {
+    console.log('Mongoose connected');
+    app.listen(port, () => {
+      console.log('Server running on port:' + port);
+    });
+  })
+  .catch(console.error);
