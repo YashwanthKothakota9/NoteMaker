8ebecf8af09205dfb08f72888a6a08d7158878bd
YashwanthK <yashwanthkothakota@gmail.com>
2024-03-02 08:00:07 +0530

    add proxy and fetch notes from server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,31 @@
 import { Button } from '@/components/ui/button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Note } from '@/models/note';
 
 function App() {
   const [notes, setNotes] = useState<Note[]>([]);
 
+  useEffect(() => {
+    async function fetchNotes() {
+      try {
+        const response = await fetch('/api/notes', {
+          method: 'GET',
+        });
+        const data = await response.json();
+        setNotes(data);
+      } catch (error) {
+        console.error(error);
+        alert('Error fetching notes');
+      }
+    }
+    fetchNotes();
+  }, []);
+
   return (
     <div>
       <h1 className="text-5xl">React App</h1>
       <Button>Click me</Button>
+      {JSON.stringify(notes)}
     </div>
   );
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
