adc0507db65bccb591eb2a37fb6a3ba020cc6e93
YashwanthK <yashwanthkothakota@gmail.com>
2024-03-02 06:26:17 +0530

    add update and delete end points

--- backend/src/controllers/notes.controller.ts
+++ backend/src/controllers/notes.controller.ts
@@ -57,3 +57,62 @@ export const createNote: RequestHandler<
     next(error);
   }
 };
+
+interface UpdateNoteParams {
+  noteId: string;
+}
+
+interface UpdateNoteBody {
+  title: string;
+  text: string;
+}
+
+export const updateNote: RequestHandler<
+  UpdateNoteParams,
+  unknown,
+  UpdateNoteBody,
+  unknown
+> = async (req, res, next) => {
+  const noteId = req.params.noteId;
+  const newTitle = req.body.title;
+  const newText = req.body.text;
+
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, 'Invalid note id');
+    }
+    if (!newTitle || !newText) {
+      throw createHttpError(400, 'Bad Request Title and text are required');
+    }
+    const note = await NoteModel.findById(noteId);
+    if (!note) {
+      throw createHttpError(404, 'Note not found');
+    }
+
+    note.title = newTitle;
+    note.text = newText;
+
+    const updatedNote = await note.save();
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteNote: RequestHandler = async (req, res, next) => {
+  const noteId = req.params.noteId;
+
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, 'Invalid note id');
+    }
+    const note = await NoteModel.findById(noteId);
+    if (!note) {
+      throw createHttpError(404, 'Note not found');
+    }
+    await note.remove();
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+};
