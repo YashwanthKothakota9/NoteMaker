54ed57afe7ef7ef4824ad2c674c3b9ba60403784
YashwanthK <yashwanthkothakota@gmail.com>
2024-03-11 10:42:29 +0530

    update code to standard build code

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -20,10 +20,7 @@ async function fetchData(input: RequestInfo, init?: RequestInit) {
 }
 
 export async function getLoggedInUser(): Promise<User> {
-  const response = await fetchData(
-    'https://notemaker-92dr.onrender.com/api/users',
-    { method: 'GET' }
-  );
+  const response = await fetchData('/api/users', { method: 'GET' });
   return response.json();
 }
 
@@ -34,16 +31,13 @@ export interface SignUpCredentials {
 }
 
 export async function signUp(credentials: SignUpCredentials): Promise<User> {
-  const response = await fetchData(
-    'https://notemaker-92dr.onrender.com/api/users/signup',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    }
-  );
+  const response = await fetchData('/api/users/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentials),
+  });
   return response.json();
 }
 
@@ -53,32 +47,26 @@ export interface LoginCredentials {
 }
 
 export async function logIn(credentials: LoginCredentials): Promise<User> {
-  const response = await fetchData(
-    'https://notemaker-92dr.onrender.com/api/users/login',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    }
-  );
+  const response = await fetchData('/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentials),
+  });
   return response.json();
 }
 
 export async function logOut() {
-  await fetchData('https://notemaker-92dr.onrender.com/api/users/logout', {
+  await fetchData('/api/users/logout', {
     method: 'POST',
   });
 }
 
 export async function fetchNotes(): Promise<Note[]> {
-  const response = await fetchData(
-    'https://notemaker-92dr.onrender.com/api/notes',
-    {
-      method: 'GET',
-    }
-  );
+  const response = await fetchData('/api/notes', {
+    method: 'GET',
+  });
   return response.json();
 }
 
@@ -88,16 +76,13 @@ export interface NoteInput {
 }
 
 export async function createNote(note: NoteInput): Promise<Note> {
-  const response = await fetchData(
-    'https://notemaker-92dr.onrender.com/api/notes',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(note),
-    }
-  );
+  const response = await fetchData('/api/notes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+  });
 
   return response.json();
 }
@@ -106,21 +91,18 @@ export async function updateNote(
   noteId: string,
   note: NoteInput
 ): Promise<Note> {
-  const response = await fetchData(
-    `https://notemaker-92dr.onrender.com/api/notes/${noteId}`,
-    {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(note),
-    }
-  );
+  const response = await fetchData(`/api/notes/${noteId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+  });
   return response.json();
 }
 
 export async function deleteNote(noteId: string): Promise<void> {
-  await fetchData(`https://notemaker-92dr.onrender.com/api/notes/${noteId}`, {
+  await fetchData(`/api/notes/${noteId}`, {
     method: 'DELETE',
   });
 }
